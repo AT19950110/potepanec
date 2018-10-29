9c244a7a54d3f9b8ed1f4981015b2756e8c8d9c4
かっしー <kasikasi00@gmail.com>
2018-10-29 19:53:27 +0900

    product_controller_rspec Editing

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::ProductsController, type: :controller do
+  describe "#show" do
+    before do
+      @product = FactoryBot.create(:product)
+    end
 
+    # 正常にレスポンスを返すこと
+    it "responds successfully" do
+      get :show, params: {id: @product.id}
+      expect(response).to be_successful
+    end
+    # 200レスポンスを返す
+    it "returns a 200 response" do
+      get :show, params: {id: @product.id}
+      expect(response).to have_http_status "200"
+    end
+  end
 end
