d47281a4761bfd701a1b852fc5ae67944584773e
かっしー <kasikasi00@gmail.com>
2018-11-15 23:12:10 +0900

    show all products

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,6 +1,10 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxonomies = Spree::Taxonomy.includes(root: :children)
-    @taxon = Spree::Taxon.find_by!(id: 1)
+    @taxon = Spree::Taxon.find_by!(params[:id])
+    @products = Spree::Product.all
+
+    # @searcher = build_searcher(params.merge(taxon: 1, include_images: true))
+    # @products = @searcher.retrieve_products
   end
 end
