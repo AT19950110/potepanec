5ad4b9bc94de6da25481ee012a826faef4bcc5f4
かっしー <kasikasi00@gmail.com>
2018-11-24 15:20:40 +0900

    Function implementation of task4

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,5 +2,17 @@ class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.friendly.find(params[:id])
     @product_properties = @product.product_properties.includes(:property)
+    @related_products = related_uniq_products_array(@product)
+  end
+
+  private
+
+  # 関連した商品の配列を重複なしでランダムに取得する
+  def related_uniq_products_array(product)
+    related_array = []
+    product.taxons.each do |taxon|
+      related_array = taxon.products + related_array
+    end
+    related_array = related_array.uniq.shuffle
   end
 end
