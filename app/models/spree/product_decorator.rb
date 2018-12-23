afa88e263cd30f560d0c60574bd283a468d5a065
かっしー <kasikasi00@gmail.com>
2018-12-23 13:53:03 +0900

    fix random

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,7 +3,6 @@ Spree::Product.class_eval do
     in_taxons(product.taxons).
       where.not(id: product.id). # 引数で渡された商品は除く
       distinct.
-      limit(number).
-      order(Arel.sql("RAND()")) # 毎回同じ関連商品を表示させない
+      sample(number)
   }
 end
