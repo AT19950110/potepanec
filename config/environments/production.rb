c02a15fbd393b350a9c4ce5f7fe8d121bc832c40
かっしー <kasikasi00@gmail.com>
2018-12-23 13:12:45 +0900

    fix production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  config.assets.digest = true
+  config.serve_static_assets = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
